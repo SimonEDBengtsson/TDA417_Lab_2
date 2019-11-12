37a94551af60e4ac45de2f58e8076f18df8dbb3d
Simon Bengtsson <ssa@tulebo.se>
2019-11-12 17:28:50 +0100

    Finished Term class.

--- src/Term.java
+++ src/Term.java
@@ -10,14 +10,39 @@ public class Term {
     }
 
     // Compares the two terms in lexicographic order by query.
-    public static Comparator<Term> byLexicographicOrder()
+    public static Comparator<Term> byLexicographicOrder(){
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                return o1.query.compareTo(o2.query);
+            }
+        };
+    }
 
     // Compares the two terms in descending order by weight.
-    public static Comparator<Term> byReverseWeightOrder()
+    public static Comparator<Term> byReverseWeightOrder(){
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                return Long.compare(o2.weight,o1.weight);
+            }
+        };
+    }
 
     // Compares the two terms in lexicographic order,
     // but using only the first k characters of each query.
-    public static Comparator<Term> byPrefixOrder(int k)
+    public static Comparator<Term> byPrefixOrder(int k){
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                int substring1Length=Math.min(k,o1.query.length());
+                int substring2Length=Math.min(k,o2.query.length());
+                String substring1=o1.query.substring(0,substring1Length);
+                String substring2=o2.query.substring(0,substring2Length);
+                return substring1.compareTo(substring2);
+            }
+        };
+    }
 
     // Returns a string representation of this term in the following format:
     // the weight, followed by whitespace, followed by the query.
