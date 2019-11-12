eb3aaaa317f15fdbd1a948ddec1223069bf647e0
Simon Bengtsson <ssa@tulebo.se>
2019-11-12 16:47:21 +0100

    Initial commit

---
+++ src/RangeBinarySearch.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class RangeBinarySearch {
+    // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
+    // Complexity: O(log N), where N is the length of the array
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator)
+
+    // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
+    // Complexity: O(log N)
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator)
+}
