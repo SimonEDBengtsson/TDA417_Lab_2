1af2732875f88ad013af139f95e90c50081f958e
Simon Bengtsson <ssa@tulebo.se>
2019-11-12 18:07:13 +0100

    Implemented RangeBinarySearch.

--- src/RangeBinarySearch.java
+++ src/RangeBinarySearch.java
@@ -2,10 +2,69 @@ import java.util.Comparator;
 
 public class RangeBinarySearch {
     // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
-    // Complexity: O(log N), where N is the length of the array
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator)
+    // Complexity: O(log N), where N is the length of the array.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+        int minBound=0;
+        int maxBound=a.length-1;
+        while (minBound<maxBound) {
+            int guess=(maxBound+minBound)/2;
+            int comparison=comparator.compare(key,a[guess]);
+            if (comparison>0){
+                minBound=guess+1;
+            }
+            else if (comparison<0){
+                maxBound=guess-1;
+            }
+            else if (minBound<guess){
+                maxBound=guess;
+            }
+            else {
+                break;
+            }
+        }
+        return minBound;
+    }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     // Complexity: O(log N)
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator)
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+        int minBound=0;
+        int maxBound=a.length-1;
+        while (minBound<maxBound) {
+            int guess=(maxBound+minBound)/2;
+            int comparison=comparator.compare(key,a[guess]);
+            if (comparison>0){
+                minBound=guess+1;
+            }
+            else if (comparison<0){
+                maxBound=guess-1;
+            }
+            else if (maxBound>guess){
+                minBound=guess;
+            }
+            else {
+                break;
+            }
+        }
+        return maxBound;
+    }
+
+    private static <Key> int binarySearch(Key[] a, Key key, Comparator<Key> comparator){
+        int minBound=0;
+        int maxBound=a.length-1;
+        while (minBound<maxBound) {
+            int guess=(maxBound+minBound)/2;
+            int comparison=comparator.compare(key,a[guess]);
+            if (comparison==0) {
+                return guess;
+            }
+            else if (comparison<0){
+                maxBound=guess-1;
+            }
+            else {
+                minBound=guess+1;
+            }
+        }
+        return a.length==1 && comparator.compare(key,a[0])==0 ? 0 : -1;
+    }
 }
