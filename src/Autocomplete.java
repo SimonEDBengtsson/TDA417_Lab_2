13010f573aa808b73829bb411b66991eb6ba3233
Simon Bengtsson <ssa@tulebo.se>
2019-11-12 19:34:11 +0100

    Added illegal argument check to autocomplete.

--- src/Autocomplete.java
+++ src/Autocomplete.java
@@ -7,6 +7,14 @@ public class Autocomplete {
     // Initializes the data structure from the given array of terms.
     // Complexity: O(N log N), where N is the number of terms
     public Autocomplete(Term[] dictionary){
+        if (dictionary==null) {
+            throw new NullPointerException();
+        }
+        for (Term term:dictionary){
+            if (term==null) {
+                throw new NullPointerException();
+            }
+        }
         this.dictionary=dictionary;
         Arrays.sort(dictionary,Term.byLexicographicOrder());
     }
@@ -14,6 +22,9 @@ public class Autocomplete {
     // Returns all terms that start with the given prefix, in descending order of weight.
     // Complexity: O(log N + M log M), where M is the number of matching terms
     public Term[] allMatches(String prefix){
+        if (prefix == null) {
+            throw new NullPointerException();
+        }
         Term term=new Term(prefix,0);
         Comparator<Term> termComparator=Term.byPrefixOrder(prefix.length());
         int firstMatch=RangeBinarySearch.firstIndexOf(dictionary,term,termComparator);
@@ -32,6 +43,9 @@ public class Autocomplete {
     // Returns the number of terms that start with the given prefix.
     // Complexity: O(log N)
     public int numberOfMatches(String prefix){
+        if (prefix == null) {
+            throw new NullPointerException();
+        }
         Term term=new Term(prefix,0);
         Comparator<Term> termComparator=Term.byPrefixOrder(prefix.length());
         int firstMatch=RangeBinarySearch.firstIndexOf(dictionary,term,termComparator);
